b830299519dc5f3e26125c0655c8027e139617b3
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-10-28 05:31:03 +0300

    Fixed output of algorythm

--- lab4/dijkstra.py
+++ lab4/dijkstra.py
@@ -8,14 +8,16 @@ def dijkstra(graph, entry_point):
     def _dijkstra(starting_edge):
         relax_edge(starting_edge)
         is_visited_node[starting_edge] = True
-        for edge_con in graph[starting_edge]:
-            if not is_visited_node[edge_con[0]]:
-                _dijkstra(edge_con[0])
+        for edge_connection in graph[starting_edge]:
+            if not is_visited_node[edge_connection[0]]:
+                _dijkstra(edge_connection[0])
 
-    if entry_point > len(graph):
-        raise ValueError("Entry point should be in graph range")
-    distance_to = {i: "inf" for i in range(len(graph))}
-    is_visited_node = {i: False for i in range(len(graph))}
+    if entry_point not in graph.keys():
+        raise ValueError("Edge with name {point} does not exist".format(point=entry_point))
+    if len(graph) == 0:
+        return {}
+    distance_to = {i: "inf" for i in graph.keys()}
+    is_visited_node = {i: False for i in graph.keys()}
     distance_to[entry_point] = 0
 
     _dijkstra(entry_point)
